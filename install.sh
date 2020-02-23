841e28a02bdd1f699b5ea5f338a93f63b2d61295
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-02-23 10:49:47 -0500

    Ensure /usr/local/bin exists
    Fixes #20

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo mkdir -p /usr/local/bin
 sudo cp locationchanger /usr/local/bin
 cp LocationChanger.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/LocationChanger.plist
