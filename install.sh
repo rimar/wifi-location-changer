0f386a7353c4b4bb42ad7ffbc240f6ab6430ee9b
Alex Rabinovich <10920796+putchi@users.noreply.github.com>
2022-01-20 18:35:50 +0200

    add support for Monterey and M1 silicon

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Copy LocationChanger files:
 sudo cp locationchanger /usr/local/bin
 cp LocationChanger.plist ~/Library/LaunchAgents/
-launchctl load ~/Library/LaunchAgents/LocationChanger.plist
+# Make locationchanger script executable:
+sudo chmod +x /usr/local/bin/locationchanger
+# Load LocationChanger.plist as a launchd daemon:
+launchctl unload -w ~/Library/LaunchAgents/LocationChanger.plist
+launchctl load -w ~/Library/LaunchAgents/LocationChanger.plist
